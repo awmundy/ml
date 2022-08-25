c5eed701dbd73715183978bc21676bb08faffb10
Andrew Mundy <awmundy@gmail.com>
2022-08-25 12:37:51 -0400

    retrieving and writing failed prediction observations

--- practice_mnist.py
+++ practice_mnist.py
@@ -3,11 +3,14 @@ from tensorflow.keras import layers
 from tensorflow import keras
 import os
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import mpld3
 
 # turn off tensorflow info messages about e.g. cpu optimization features (turn
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 np.set_printoptions(suppress=True, formatter={'float_kind':'{:f}'.format}) # no scientific notation
-#
+
 
 
 def prep_data(train_images, test_images):
@@ -17,6 +20,33 @@ def prep_data(train_images, test_images):
     test_images = test_images.astype("float32") / 255
 
     return train_images,test_images
+
+def value_counts_np(np_array, sort_by_count=True):
+    value_counts = np.unique(np_array, return_counts=True)
+    value_counts = pd.Series(data=value_counts[1], index=value_counts[0])
+    value_counts.name = 'Value Counts'
+
+    if sort_by_count:
+        value_counts.sort_values(ascending=False, inplace=True)
+
+    return value_counts
+
+def write_output_html(fail_dist):
+    output_path = os.path.expanduser('~/Desktop/test2.html')
+    if os.path.exists(output_path):
+        os.remove(output_path)
+    with open(output_path, 'a') as report:
+        # todo try building a figure that also shows what we want
+        # t = plt.imshow(fail_images[0].reshape(28,28), cmap=plt.cm.binary)
+        # mpld3.save_html(t, report)
+
+        val_counts_1 = pd.DataFrame(fail_dist)
+        val_counts_1.index.name = 'Category'
+        val_counts_1.reset_index(inplace=True)
+        val_counts_2 = val_counts_1.sort_values(by='Category')
+        header = '<b> Incorrect predictions </b> <br><br>'
+        report.write(header + val_counts_1.to_html(index=False) +
+                     "<br><br>" + val_counts_2.to_html(index=False))
 
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 train_images, test_images = prep_data(train_images, test_images)
@@ -31,6 +61,23 @@ model.compile(optimizer="rmsprop",
               metrics=["accuracy"])
 
 model.fit(train_images, train_labels, epochs=5, batch_size=128)
+
+# measure accuracy against test data
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+pred = model.predict(test_images)
+max_pred = []
+for idx, obs in enumerate(pred):
+    max_pred += [np.argmax(obs)]
+
+fail_idxs = np.nonzero(max_pred != test_labels)[0]
+fail_labels = test_labels[fail_idxs]
+fail_images = test_images[fail_idxs]
+fail_dist = value_counts_np(fail_labels)
+print(fail_dist)
+
+write_output_html(fail_dist)
+
 
 
 # pre process
