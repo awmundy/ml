7aed769dd67727d841537899418d828b73e7b557
Andrew Mundy <awmundy@gmail.com>
2022-09-01 16:22:33 -0400

    added log callback, included that in report and cleaned up report/paths

--- practice_mnist.py
+++ practice_mnist.py
@@ -71,6 +71,25 @@ def get_history_df(history, pretty_cols=False):
     
     return history_df
 
+def build_training_log_html(training_log_path):
+    log = pd.read_csv(training_log_path, dtype=float)
+    html =  f"""
+    <html>
+      <head>
+      </head>
+      <body>
+        <h2>Training Log</h2>
+        <table style="width:100">
+            <tr>
+              <td>{log.to_html(index=False)}</td>
+            </tr>
+          </table>
+      </body>
+    </html>
+    """
+
+    return html
+
 def build_fail_counts_html(fail_dist, pred_accuracy):
 
     # todo move this out of here
@@ -148,11 +167,13 @@ def build_training_plot_html(history, plot_type):
     df = get_history_df(history)
 
     if plot_type == 'accuracy':
+        title = 'Training Accuracy'
         y_col_1 = 'accuracy'
         y_col_1_label = 'Training accuracy'
         y_col_2 = 'val_accuracy'
         y_col_2_label = 'Validation accuracy'
     elif plot_type == 'loss':
+        title = 'Training Loss'
         y_col_1 = 'loss'
         y_col_1_label = 'Training loss'
         y_col_2 = 'val_loss'
@@ -175,6 +196,7 @@ def build_training_plot_html(history, plot_type):
     # plot
     plt.plot(df['epoch'], df[y_col_1], "bo", label=y_col_1_label)
     plt.plot(df['epoch'], df[y_col_2], "b", label=y_col_2_label)
+    plt.title(title)
     plt.xlabel("Epochs")
     plt.ylabel(plot_type)
     plt.legend()
@@ -190,7 +212,7 @@ def build_fail_images_plot_html(fail_images):
     plot_rows = 2
     plot_cols = 5
 
-    fig, ax_list = plt.subplots(plot_rows, plot_cols, sharex="col", sharey="row", figsize=(8, 8))
+    fig, ax_list = plt.subplots(plot_rows, plot_cols, sharex="col", sharey="row", figsize=(4, 4))
     fig.subplots_adjust(left=0.05, right=0.95, bottom=0.05, top=0.95,
                         hspace=0.0, wspace=0.0)
     i = 0
@@ -232,33 +254,46 @@ def get_failing_predictions(pred_labels, test_data, test_labels):
 
 def read_model_graph_as_html(model_graph_path):
     data_uri = base64.b64encode(open(model_graph_path, 'rb').read()).decode('utf-8')
-    html_graph = '<img src="data:image/png;base64,{0}">'.format(data_uri)
+    html = f"""
+        <br>
+        <h2>Model Graph</h2>
+        <img src="data:image/png;base64,{data_uri}">
+        <br>
+    """
+    # html_graph = '''<img src="data:image/png;base64,{0}">'''.format(data_uri)
+    return html
 
-    return html_graph
+def write_report(output_paths, model, fail_images, fail_dist, pred_accuracy, history):
 
-def write_report(model, fail_images, fail_dist, pred_accuracy, history):
-
-    report_path = os.path.expanduser('~/Desktop/test2.html')
-    model_graph_path = os.path.expanduser('~/Desktop/graph.png')
+    report_path = output_paths['report']
+    model_graph_path = output_paths['model_graph']
+    training_log_path = output_paths['training_log']
     write_model_graph(model, model_graph_path)
 
     if os.path.exists(report_path):
         os.remove(report_path)
-    html_graph = read_model_graph_as_html(model_graph_path)
-    html_fail_counts = build_fail_counts_html(fail_dist, pred_accuracy,)
+    html_model_graph = read_model_graph_as_html(model_graph_path)
+    html_fail_counts = build_fail_counts_html(fail_dist, pred_accuracy)
     html_accuracy = build_training_plot_html(history, 'accuracy')
     html_loss = build_training_plot_html(history, 'loss')
     html_fail_images = build_fail_images_plot_html(fail_images)
+    training_log = build_training_log_html(training_log_path)
 
     with open(report_path, 'a') as report:
         report.write(html_accuracy)
         report.write(html_loss)
-        report.write(html_fail_images)
+        report.write(training_log)
+        report.write(html_model_graph)
         report.write(html_fail_counts)
-        report.write(html_graph)
+        report.write(html_fail_images)
         report.close()
         print('done writing report')
 
+usr_path = os.path.expanduser('~/')
+output_paths = {'training_log': f'{usr_path}/Desktop/training_log.csv',
+                'model_graph': f'{usr_path}/Desktop/model_graph.png',
+                'report': f'{usr_path}/Desktop/test2.html'
+                }
 
 # todo write data assertions and wrap in a function (e.g. output layer has same # of nodes as # of categories)
 # todo plot benchmark line on accuracy graphs
@@ -284,7 +319,8 @@ history = model.fit(train_data,
                     shuffle=False,
                     epochs=3,
                     batch_size=128,
-                    validation_split=.2)
+                    validation_split=.2,
+                    callbacks=keras.callbacks.CSVLogger(output_paths['training_log']))
 
 pred_loss, pred_accuracy = model.evaluate(test_data, test_labels)
 
@@ -292,6 +328,6 @@ pred_loss, pred_accuracy = model.evaluate(test_data, test_labels)
 # report
 pred_labels = get_predicted_labels(model, test_data)
 fail_images, fail_dist = get_failing_predictions(pred_labels, test_data, test_labels)
-write_report(model, fail_images, fail_dist, pred_accuracy, history)
+write_report(output_paths, model, fail_images, fail_dist, pred_accuracy, history)
 
 
