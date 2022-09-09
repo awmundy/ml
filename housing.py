daae3003de5893a17239658fdf65e488cf15cc86
Andrew Mundy <awmundy@gmail.com>
2022-09-09 14:12:32 -0400

    corr matrix heatmap, stared testing other categorical cols

--- housing.py
+++ housing.py
@@ -9,7 +9,11 @@ import statsmodels.api as sm
 from sklearn.model_selection import train_test_split
 import numpy as np
 import pprint
+import matplotlib.pyplot as plt
+import seaborn as sn
+
 import ml.shared as shared
+
 
 # turn off tensorflow info messages about e.g. cpu optimization features
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
@@ -58,12 +62,12 @@ def alter_dtypes(df):
 
     return df
 
-def one_hot_categoricals(df):
-    categorical_cols = ['region']
-    for col in categorical_cols:
-        dum = pd.get_dummies(df[col], prefix=col)
-        dum.drop(dum.columns[-1], axis=1, inplace=True) # drop last one to handle collinearity
-        df = pd.concat([df, dum], axis=1)
+def one_hot_categoricals(df, categorical_cols):
+    if len(categorical_cols) > 0:
+        for col in categorical_cols:
+            dum = pd.get_dummies(df[col], prefix=col)
+            dum.drop(dum.columns[-1], axis=1, inplace=True) # drop last one to handle collinearity
+            df = pd.concat([df, dum], axis=1)
 
     return df
 
@@ -82,9 +86,9 @@ def build_prediction_error_html(pred_error, ols_error):
       <head>
       </head>
       <body>
-        <h2>Neural Net Prediction Error</h2>
+        <h2>Neural Net Prediction Error per Obs</h2>
         {pred_error}
-        <h2>OLS Prediction Error</h2>
+        <h2>OLS Prediction Error per Obs</h2>
         {ols_error}
       </body>
     </html>
@@ -100,7 +104,7 @@ def convert_dict_to_html(cfg):
 
     return html
 
-def write_report(output_paths, model, pred_error, history, ols_error, cfg):
+def write_report(output_paths, model, pred_error_per_obs, history, ols_error, cfg):
 
     metric = cfg['metrics']
     assert len(metric) == 1
@@ -110,6 +114,7 @@ def write_report(output_paths, model, pred_error, history, ols_error, cfg):
     model_graph_path = output_paths['model_graph']
     training_log_path = output_paths['training_log']
     shared.write_model_graph(model, model_graph_path)
+    corr_heatmap_path = output_paths['corr_heatmap']
 
     if os.path.exists(report_path):
         os.remove(report_path)
@@ -117,15 +122,17 @@ def write_report(output_paths, model, pred_error, history, ols_error, cfg):
     html_accuracy = shared.build_training_plot_html(history, metric)
     html_loss = shared.build_training_plot_html(history, 'loss')
     html_training_log = shared.build_training_log_html(training_log_path)
-    html_pred_error = build_prediction_error_html(pred_error, ols_error)
+    html_pred_error = build_prediction_error_html(pred_error_per_obs, ols_error)
     html_cfg = convert_dict_to_html(cfg)
+    html_corr_heatmap = shared.read_image_as_html(corr_heatmap_path, 'Correlation Matrix')
 
     with open(report_path, 'a') as report:
         report.write(html_cfg)
         report.write(html_pred_error)
         report.write(html_accuracy)
         report.write(html_loss)
-        report.write(html_training_log)
+        # report.write(html_training_log)
+        report.write(html_corr_heatmap)
         report.write(html_model_graph)
         report.close()
         print('done writing report')
@@ -147,10 +154,17 @@ def get_ols_error(train_labels, train_data, test_data):
 
     return ols_error
 
+def write_correlation_matrix_heatmap(train_data, train_labels, out_path):
+    corr_mat = pd.concat([train_data, train_labels], axis=1).corr()
+    corr_mat = corr_mat.round(2)
+    fig = plt.figure(figsize = (15, 15))
+    sn.heatmap(corr_mat, annot=True)
+    plt.savefig(out_path)
 
 usr_path = os.path.expanduser('~/')
 output_paths = {'training_log': f'{usr_path}/Desktop/housing/training_log.csv',
                 'model_graph': f'{usr_path}/Desktop/housing/model_graph.png',
+                'corr_heatmap': f'{usr_path}/Desktop/housing/corr_heatmap.png',
                 'report': f'{usr_path}/Desktop/housing/housing_report.html'
                 }
 
@@ -163,31 +177,44 @@ output_paths = {'training_log': f'{usr_path}/Desktop/housing/training_log.csv',
 cfg = {'layers': [['relu', 64],
                   ['relu', 64],
                   [None, 1]],
-       'epochs': 10,
+       'epochs': 50,
        'batch_size': 32,
-       'loss': 'mse',
+       'loss': 'mae',
        'metrics': ['mae'],
        'normalized_features': ['sqft']
        }
 
+shared.use_cpu_and_make_results_reproducible()
 download_data()
 df = pd.read_excel('/home/amundy/Documents/census_data/housing/2021_mfr_house_puf.xls', dtype=str)
 df['constant'] = 1
 df.columns = df.columns.str.lower()
 df = alter_dtypes(df)
-df = one_hot_categoricals(df)
+categorical_cols = \
+    [
+        # 'region',
+        'sections',
+        # 'finaldest',
+        # 'footings',
+        # 'secured',
+        # 'bedrooms'
+        ]
+df = one_hot_categoricals(df, categorical_cols)
+features = ['constant', 'sqft']
+for col in categorical_cols:
+    features += [x for x in df if col + '_' in x]
+cfg['features'] = features
 
 train_data, train_labels, test_data, test_labels = \
     split_to_train_test_and_data_labels(df, .2, 'price')
 
 
-features = ['constant', 'sqft'] + [x for x in df if 'region_' in x]
-cfg['features'] = features
 train_data = train_data[features].copy()
 test_data = test_data[features].copy()
+write_correlation_matrix_heatmap(train_data, train_labels, output_paths['corr_heatmap'])
 ols_error = get_ols_error(train_labels, train_data, test_data)
 
-if len(cfg['normalized_features']) > 0:
+if 'normalized_features' in cfg.keys():
     train_data = normalize(train_data, cfg['normalized_features'])
 
 model = keras.Sequential()
@@ -208,7 +235,8 @@ history = model.fit(train_data,
                     )
 
 pred_loss, pred_error = model.evaluate(test_data, test_labels)
-write_report(output_paths, model, pred_error, history, ols_error, cfg)
+pred_error_per_obs = pred_error / len(test_data)
+write_report(output_paths, model, pred_error_per_obs, history, ols_error, cfg)
 
 ### NOTES ###
 # explanatory variables
