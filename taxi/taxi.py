b07da47a76be00bb569c21196241016524eb3b38
Andrew Mundy <awmundy@gmail.com>
2022-11-01 11:59:07 -0400

    start prepping data and writing out plots, placeholder report function

--- taxi/taxi.py
+++ taxi/taxi.py
@@ -54,4 +54,51 @@ def get_dtypes(dataset):
 
     return dtypes, dt_cols
 
+
+
+def convert_store_and_fwd_flag_to_float(df):
+    df['store_and_fwd_flag'] = df['store_and_fwd_flag'].replace({'Y':1.0, 'N': 0.0})
+
+    return df
+
+def remove_outlier_long_trips(df):
+    trip_cap = 7200
+    msk = df['trip_duration'] >= trip_cap
+    print(f'removing {msk.sum()} trips lasting {7200/60/60} hours or longer')
+    df = df[~msk].copy()
+
+    return df
+
+def write_report():
+    pass
+    # import plotly.express as px
+    # fig = px.histogram(train, x='trip_duration')
+    #
+    # hist_html = fig.to_html()
+    # with open(f'{usr_dir}/Documents/ml_taxi/test.html', 'w') as report:
+    #     report.write(hist_html)
+
+usr_dir = os.path.expanduser('~')
+train_path = f'{usr_dir}/Documents/ml_taxi/train.csv'
+test_path = f'{usr_dir}/Documents/ml_taxi/test.csv'
+train_histogram_path = f'{usr_dir}/Documents/ml_taxi/histogram_train.png'
+test_histogram_path = f'{usr_dir}/Documents/ml_taxi/histogram_test.png'
 shared.use_cpu_and_make_results_reproducible()
+
+
+
+dtypes, dt_cols = get_dtypes('test')
+test = pd.read_csv(test_path, dtype=dtypes, parse_dates=dt_cols)
+# convert Y/N col to float
+test = convert_store_and_fwd_flag_to_float(test)
+assert test.notnull().all().all()
+dtypes, dt_cols = get_dtypes('train')
+train = pd.read_csv(train_path, dtype=dtypes, parse_dates=dt_cols)
+train = convert_store_and_fwd_flag_to_float(train)
+train = remove_outlier_long_trips(train)
+
+
+write_histogram(test, test_histogram_path)
+write_histogram(train, train_histogram_path)
+
+
