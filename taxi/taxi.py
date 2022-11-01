9d96f90277cae6ee8f7b8250cca1b9e1d9182ce3
Andrew Mundy <awmundy@gmail.com>
2022-11-01 12:42:06 -0400

    some imports cleanup, added gpd

--- taxi/taxi.py
+++ taxi/taxi.py
@@ -2,14 +2,13 @@ import os
 from tensorflow.keras import layers
 from tensorflow import keras
 import pandas as pd
-import requests
 import statsmodels.api as sm
-from sklearn.model_selection import train_test_split
 import numpy as np
 import pprint
 import matplotlib.pyplot as plt
 import seaborn as sn
 from statsmodels.stats.outliers_influence import variance_inflation_factor as get_vif
+import geopandas as gpd
 import ml.shared as shared
 
 
