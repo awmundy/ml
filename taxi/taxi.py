eea56e86812556b4f5fb949af42666bd8f443894
Andrew Mundy <awmundy@gmail.com>
2022-11-01 15:56:45 -0400

    wrap scatterplot function, cleanup

--- taxi/taxi.py
+++ taxi/taxi.py
@@ -111,61 +111,66 @@ def remove_outlier_lat_long_trips(df):
 
     return df
 
+def write_pickup_dropoff_scatterplot_map(train, train_map_path):
+    # subset bc the viz gets unreadable due to dropoff points overlapping pickup
+    df = train.sample(10000, random_state=1).copy()
 
+    # read in shape file that contains map boundaries
+    nyc_geo = gpd.read_file(nyc_boundary_path)
+
+    # construct geopandas dataframes
+    pickup_gdf = gpd.GeoDataFrame(df.copy(), geometry=gpd.points_from_xy(df['pickup_longitude'],
+                                                                         df['pickup_latitude']))
+    dropoff_gdf = gpd.GeoDataFrame(df.copy(), geometry=gpd.points_from_xy(df['dropoff_longitude'],
+                                                                          df['dropoff_latitude']))
+    # add the boundaries and data to a plot
+    ax = nyc_geo.plot(facecolor='whitesmoke', edgecolor='black', figsize=(12,12))
+    pickup_gdf.plot(ax=ax, color='purple', marker='^', label='pickup', alpha=.1, markersize=10)
+    dropoff_gdf.plot(ax=ax, color='darkorange', marker='v', label='dropoff', alpha=.1, markersize=10)
+
+    # zoom in on the area these points are in
+    minx = min(pickup_gdf['pickup_longitude'].min(), dropoff_gdf['dropoff_longitude'].min()) - .01
+    maxx = min(pickup_gdf['pickup_longitude'].max(), dropoff_gdf['dropoff_longitude'].max()) +.01
+    miny = min(pickup_gdf['pickup_latitude'].min(), dropoff_gdf['dropoff_latitude'].min()) - .01
+    maxy = min(pickup_gdf['pickup_latitude'].max(), dropoff_gdf['dropoff_latitude'].max()) + .01
+    ax.set_xlim(minx, maxx)
+    ax.set_ylim(miny, maxy)
+
+    # make legend
+    lines = [Line2D([0], [0], linestyle="none", marker="s", markersize=15,
+             markerfacecolor=a.get_facecolor())for a in ax.collections[1:]]
+    labels = [t.get_label() for t in ax.collections[1:]]
+    ax.legend(lines, labels)
+
+    # write out
+    plt.savefig(train_map_path)
 
 usr_dir = os.path.expanduser('~')
 train_path = f'{usr_dir}/Documents/ml_taxi/train.csv'
 test_path = f'{usr_dir}/Documents/ml_taxi/test.csv'
+nyc_boundary_path = f'{usr_dir}/Documents/ml_taxi/nyc_borough_geo_files/geo_export_d66f2294-5e4d-4fd3-92f2-cdb0a859ef48.shp'
 train_histogram_path = f'{usr_dir}/Documents/ml_taxi/histogram_train.png'
 test_histogram_path = f'{usr_dir}/Documents/ml_taxi/histogram_test.png'
+train_map_path = f'{usr_dir}/Documents/ml_taxi/map_train.png'
 shared.use_cpu_and_make_results_reproducible()
 turn_off_scientific_notation()
 
 
 dtypes, dt_cols = get_dtypes('test')
 test = pd.read_csv(test_path, dtype=dtypes, parse_dates=dt_cols)
-# convert Y/N col to float
 test = convert_store_and_fwd_flag_to_float(test)
 test = remove_0_passenger_count_trips(test)
 test = remove_outlier_lat_long_trips(test)
 assert test.notnull().all().all()
+
 dtypes, dt_cols = get_dtypes('train')
 train = pd.read_csv(train_path, dtype=dtypes, parse_dates=dt_cols)
 train = convert_store_and_fwd_flag_to_float(train)
 train = remove_outlier_long_trips(train)
 train = remove_0_passenger_count_trips(train)
 train = remove_outlier_lat_long_trips(train)
+assert train.notnull().all().all()
 
 write_histogram(test, test_histogram_path)
 write_histogram(train, train_histogram_path)
-
-# plt.scatter(x=test['pickup_longitude'], y=test['pickup_latitude'])
-# plt.show()
-
-df = train.head(100000).copy()
-nyc_geo = gpd.read_file('/home/amundy/Documents/ml_taxi/nyc_borough_geo_files/geo_export_d66f2294-5e4d-4fd3-92f2-cdb0a859ef48.shp')
-pickup_gdf = gpd.GeoDataFrame(df.copy(), geometry=gpd.points_from_xy(df['pickup_longitude'],
-                                                          df['pickup_latitude']))
-dropoff_gdf = gpd.GeoDataFrame(df.copy(), geometry=gpd.points_from_xy(df['dropoff_longitude'],
-                                                          df['dropoff_latitude']))
-# print(pickup_gdf.head())
-# print(dropoff_gdf.head())
-
-# fig, ax = plt.subplots()
-# nyc_geo.plot(ax=ax)
-ax = nyc_geo.plot(facecolor='azure', edgecolor='black')
-pickup_gdf.plot(ax=ax, color='darkblue', marker='^', label='pickup', alpha=.1)
-dropoff_gdf.plot(ax=ax, color='crimson', marker='v', label='dropoff', alpha=.1)
-minx = min(pickup_gdf['pickup_longitude'].min(), dropoff_gdf['dropoff_longitude'].min()) - .01
-maxx = min(pickup_gdf['pickup_longitude'].max(), dropoff_gdf['dropoff_longitude'].max()) +.01
-miny = min(pickup_gdf['pickup_latitude'].min(), dropoff_gdf['dropoff_latitude'].min()) - .01
-maxy = min(pickup_gdf['pickup_latitude'].max(), dropoff_gdf['dropoff_latitude'].max()) + .01
-ax.set_xlim(minx, maxx)
-ax.set_ylim(miny, maxy)
-lines = [
-    Line2D([0], [0], linestyle="none", marker="s", markersize=10, markerfacecolor=a.get_facecolor())
-    for a in ax.collections[1:]
-]
-labels = [t.get_label() for t in ax.collections[1:]]
-ax.legend(lines, labels)
-plt.show()
+write_pickup_dropoff_scatterplot_map(train, train_map_path)
